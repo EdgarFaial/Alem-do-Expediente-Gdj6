e392aff5b883cb8f0b492582bce2e1fe309066fc
RhanieryCorreia <haniery.rh@gmail.com>
2026-04-17 09:05:04 -0300

    Add files via upload

--- canvas_layer.gd
+++ canvas_layer.gd
@@ -1,16 +1,13 @@
 extends CanvasLayer
 
-@onready var label = $Panel/Label
 @onready var panel = $Panel
-@onready var timer = $Timer
+@onready var label = $Panel/Label
 
-func _ready():
-	panel.visible = false
-
-func show_text(text: String):
+func set_card_text(text: String):
 	label.text = text
-	panel.visible = true
-	timer.start(3.0)  # some após 3 segundos
 
-func _on_timer_timeout():
+func show_card():
+	panel.visible = true
+
+func hide_card():
 	panel.visible = false
--- carta.gd
+++ carta.gd
@@ -1,12 +1,23 @@
 extends Area2D
 
-@export var carta_texto: String = "Texto da carta..."
-@export var texto_ui: Control
+@onready var label = $CanvasLayer/Label   # Certifique-se que o caminho está correto
 
 func _ready():
 	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	
+	if label:  # Previne erro caso label seja null
+		label.visible = false
+	else:
+		print("Erro: Label não encontrado! Verifique o caminho.")
 
 func _on_body_entered(body):
-	if body.name == "CharacterBody2D":
-		texto_ui.show_text("Ah, finalmente eu consegui sair daquela cidade grotesca \n foram infinitas horas repondo caixas nas prateleiras do supermercado, eu não podia continuar \n não podia terminar como todos da minha fámilia terminaram... \n Talvez aqui eu possa encontrar um pouco de sossego.")
-		queue_free()
+	# Verifica se o corpo que entrou está no grupo "CharacterBody2D"
+	if body.is_in_group("CharacterBody2D"):
+		if label:
+			label.visible = true
+
+func _on_body_exited(body):
+	if body.is_in_group("CharacterBody2D"):
+		if label:
+			label.visible = false
